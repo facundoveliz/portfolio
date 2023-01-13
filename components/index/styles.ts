bc7bb038d164a979aac1ce60a15afeead961fa61
Facundo Veliz <facundoveliz9@gmail.com>
2023-01-13 20:44:00 -0300

    changed header sizes

--- components/index/styles.ts
+++ components/index/styles.ts
@@ -5,11 +5,7 @@ import { MdOutlineMail } from 'react-icons/md';
 export const Wrapper = styled.div``;
 
 export const Introduction = styled.div`
-  margin-top: 60px;
-  @media (max-width: 650px) {
-    margin-top: 0;
-    height: 100vh;
-  }
+  margin-top: 64px;
   display: flex;
   flex-direction: column;
   justify-content: center;
